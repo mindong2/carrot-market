c53bbc75791cda4a4907986c74fcd58959729db8
Dongmin Kim <qlsnaos12@naver.com>
2023-06-20 18:32:14 +0900

    question, answer

---
+++ pages/api/post/[id].tsx
@@ -0,0 +1,57 @@
+import client from "@/libs/server/client";
+import withHandler, { ResponseType } from "@/libs/server/withHandler";
+import { withApiSession } from "@/libs/server/withSession";
+import { NextApiRequest, NextApiResponse } from "next";
+
+async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
+  const {
+    query: { id },
+  } = req;
+
+  const post = await client.post.findUnique({
+    where: {
+      id: Number(id),
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+      answers: {
+        select: {
+          answer: true,
+          id: true,
+          user: {
+            select: {
+              id: true,
+              name: true,
+              avatar: true,
+            },
+          },
+        },
+      },
+      _count: {
+        select: {
+          answers: true,
+        },
+      },
+    },
+  });
+
+  res.status(200).json({ success: true, post });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["GET"],
+    handler,
+  })
+);
+
+/*
+  prisma client find API 관련 링크
+  https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#filter-conditions-and-operators
+*/
